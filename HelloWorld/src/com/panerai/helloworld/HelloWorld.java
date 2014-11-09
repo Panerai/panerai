021c6426c8d09110513d28a77409aae0f7f37c6e
Panerai <daibingpei@csair.com>
2014-11-09 22:18:39 +0800

    HelloWorld

--- HelloWorld/src/com/panerai/helloworld/HelloWorld.java
+++ HelloWorld/src/com/panerai/helloworld/HelloWorld.java
@@ -5,7 +5,5 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld");
-		System.out.println("HelloWorld");
 	}
-
 }
